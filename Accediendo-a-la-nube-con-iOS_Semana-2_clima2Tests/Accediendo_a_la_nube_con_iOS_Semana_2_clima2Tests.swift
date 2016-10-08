35c00706d3c480698c20d1aeff582bf3c12919e4
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-08 18:52:45 -0500

    Initial Commit

---
+++ Accediendo-a-la-nube-con-iOS_Semana-2_clima2Tests/Accediendo_a_la_nube_con_iOS_Semana_2_clima2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Accediendo_a_la_nube_con_iOS_Semana_2_clima2Tests.swift
+//  Accediendo-a-la-nube-con-iOS_Semana-2_clima2Tests
+//
+//  Created by Juan Carlos Carbajal Ipenza on 8/10/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import XCTest
+@testable import Accediendo_a_la_nube_con_iOS_Semana_2_clima2
+
+class Accediendo_a_la_nube_con_iOS_Semana_2_clima2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
