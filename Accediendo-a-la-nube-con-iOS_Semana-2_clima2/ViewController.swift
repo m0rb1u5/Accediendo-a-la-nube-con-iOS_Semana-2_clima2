f2b144c30bde786a3bf3416cf3242f3177afaa18
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-08 22:54:18 -0500

    Clase "Ejemplo. Parte 3".

--- Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
+++ Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        ciudades.append(["Caracas", "VEXX0008"])
-        ciudades.append(["Paris", "FRXX0076"])
-        ciudades.append(["Grenoble", "FRXX0153"])
+        ciudades.append(["Caracas", "395269"])
+        ciudades.append(["Paris", "615702"])
+        ciudades.append(["Grenoble", "593720"])
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -35,13 +35,20 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return self.ciudades[row][0]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let urls = "https://query.yahooapis.com/v1/public/yql?format=json&q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20u%20=%20%27c%27%20and%20location%20=%20%27"
+        let urls = "https://query.yahooapis.com/v1/public/yql?format=json&q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20u%20=%20%27c%27%20and%20woeid%20=%20%27"
         let url: NSURL? = NSURL(string: urls + self.ciudades[row][1] + "%27")
         let datos: NSData? = NSData(contentsOf: url! as URL)
         do {
             let json = try JSONSerialization.jsonObject(with: datos! as Data, options: JSONSerialization.ReadingOptions.mutableLeaves)
             let dico1 = json as! NSDictionary
             let dico2 = dico1["query"] as! NSDictionary
+            let dico3 = dico2["results"] as! NSDictionary
+            let dico4 = dico3["channel"] as! NSDictionary
+            let dico5 = dico4["location"] as! NSDictionary
+            self.ciudad.text = dico5["city"] as! NSString as String
+            let dico6 = dico4["item"] as! NSDictionary
+            let dico7 = dico6["condition"] as! NSDictionary
+            self.temperatura.text = dico7["temp"] as! NSString as String + " ºC"
         }
         catch _ {
             
