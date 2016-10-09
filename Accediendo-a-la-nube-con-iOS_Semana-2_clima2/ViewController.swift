e4eb600fc159307a23e7dc2241835c736e18e9b0
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-08 19:56:35 -0500

    Curso "Ejemplo. Parte 2"

--- Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
+++ Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
@@ -31,5 +31,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return self.ciudades.count
     }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.ciudades[row][0]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let urls = "https://query.yahooapis.com/v1/public/yql?format=json&q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20u%20=%20%27c%27%20and%20location%20=%20%27"
+        let url: NSURL? = NSURL(string: urls + self.ciudades[row][1] + "%27")
+        let datos: NSData? = NSData(contentsOf: url! as URL)
+        do {
+            let json = try JSONSerialization.jsonObject(with: datos! as Data, options: JSONSerialization.ReadingOptions.mutableLeaves)
+            let dico1 = json as! NSDictionary
+            let dico2 = dico1["query"] as! NSDictionary
+        }
+        catch _ {
+            
+        }
+    }
 }
 
