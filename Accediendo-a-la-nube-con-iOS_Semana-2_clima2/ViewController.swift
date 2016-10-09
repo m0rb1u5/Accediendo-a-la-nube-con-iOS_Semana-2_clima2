e9249eaa7e6d799c0154c0bf46c93ab4582f5150
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-08 19:18:26 -0500

    Clase "Ejemplo Parte 1".

--- Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
+++ Accediendo-a-la-nube-con-iOS_Semana-2_clima2/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var ciudad: UILabel!
+    @IBOutlet weak var temperatura: UILabel!
+    
+    var ciudades: Array<Array<String>> = Array<Array<String>>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ciudades.append(["Caracas", "VEXX0008"])
+        ciudades.append(["Paris", "FRXX0076"])
+        ciudades.append(["Grenoble", "FRXX0153"])
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
